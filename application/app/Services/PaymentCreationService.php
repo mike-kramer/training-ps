321babbd625d1bcea78459f75607a69c46df85dc
Daniil <daniil@gmail.com>
2026-08-28 19:58:20 +0500

    Add audit log to payment creation

--- application/app/Services/PaymentCreationService.php
+++ application/app/Services/PaymentCreationService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Contracts\AuditLogContract;
 use App\Contracts\SignatureContract;
 use App\Data\Payments\PaymentData;
 use App\Exceptions\InvalidSignatureException;
@@ -12,20 +13,31 @@ class PaymentCreationService
 {
     public function __construct(
         readonly private SignatureContract $signService,
+        readonly private AuditLogContract  $auditLogService
     )
     {
 
     }
+
     public function createPayment(PaymentData $data, string $signature): string
     {
         $cashbox = Cashbox::findOrFail($data->cashbox_id);
-        if (!$this->signService->checkSignature((array) $data, $cashbox->secret_key, $signature)) {
+        if (!$this->signService->checkSignature((array)$data, $cashbox->secret_key, $signature)) {
             throw new InvalidSignatureException();
         }
         $payment = Payment::create([
-            ...(array) $data,
+            ...(array)$data,
             "status" => Payment::STATUS_PENDING,
         ]);
+        $this->auditLogService->log(
+            "payment-created",
+            null,
+            null,
+            $cashbox->id,
+            parameters: [
+                "order_id" => $payment->order_id,
+            ]
+        );
         return config("app.url") . "/process-payment/{$payment->id}";
     }
 }
